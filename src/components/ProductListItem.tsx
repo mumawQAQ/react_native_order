7ec76cb49c047bdda7a2b001ec489a6112cf3a71
muma <GUW18@pitt.edu>
2024-04-14 06:13:47 -0400

    Fix the props and default img

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,11 +1,17 @@
 import {Image, StyleSheet, View} from "react-native";
 import Colors from "@/src/constants/Colors";
 import {Text} from "@/src/components/Themed";
+import {Product} from "@/src/types";
 
-const ProductListItem = ({product}) => {
+type ProductListItemProps = {
+    product: Product;
+}
+
+const defaultImg = "'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
+const ProductListItem = ({product}: ProductListItemProps) => {
     return (
         <View style={styles.container}>
-            <Image source={{uri: product.image}} style={styles.image}/>
+            <Image source={{uri: product.image || defaultImg}} style={styles.image}/>
             <Text style={styles.title}>{product.name}</Text>
             <Text style={styles.price}>${product.price}</Text>
         </View>
