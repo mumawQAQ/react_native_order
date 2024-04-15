925a3488b84da6a52ded00905c9098de5550d3b5
muma <GUW18@pitt.edu>
2024-04-15 12:47:16 -0400

    Order state change

--- src/app/(admin)/_layout.tsx
+++ src/app/(admin)/_layout.tsx
@@ -39,7 +39,7 @@ export default function TabLayout() {
                 }}
             />
             <Tabs.Screen
-                name="two"
+                name="orders"
                 options={{
                     title: 'Order',
                     tabBarIcon: ({color}) => <TabBarIcon name="list" color={color}/>,
